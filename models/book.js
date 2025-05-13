27156ca22ca9a08d5bc17985bd6e250db762e4ad
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-05-13 08:03:32 +0300

    Added Some Notes about Mongoose ✔

--- models/book.js
+++ models/book.js
@@ -1,5 +1,4 @@
-﻿// models/bookModel.js
-const mongoose = require('mongoose');
+﻿const mongoose = require('mongoose');
 
 // Define the schema for the Book model
 const bookSchema = new mongoose.Schema({
@@ -23,3 +22,18 @@ const bookSchema = new mongoose.Schema({
 const Book = mongoose.models.Book || mongoose.model('Book', bookSchema);
 
 module.exports = Book;
+
+
+// Some Notes about Mongoose
+//====================================
+// Mongoose is an Object Data Modeling (ODM) library for MongoDB and Node.js. It provides a higher-level abstraction for interacting with MongoDB, making it easier to work with data by defining schemas and models.
+
+// Key features of Mongoose:
+//====================================
+// (1) Schemas: Define the structure of documents within a collection.
+
+// (2) Models: Represent a MongoDB collection, and provide methods for querying and manipulating data.
+
+// (3) Validation: Enforces data validation rules to ensure consistency in data.
+
+// (4) Middleware: Supports hooks to run code before or after certain operations (e.g., before saving a document).
