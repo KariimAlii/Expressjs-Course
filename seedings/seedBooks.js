d8b443ab5b1b5000649b698079ae20824c0e184f
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-05-13 07:57:30 +0300

    Seeding Books data if Book Collection is empty ✔

---
+++ seedings/seedBooks.js
@@ -0,0 +1,30 @@
+﻿// seed.js
+const Book = require('../models/Book');
+
+const seedBooks = async () => {
+    const count = await Book.countDocuments();
+    if (count === 0) {
+        await Book.insertMany([
+            {
+                title: '1984',
+                author: 'George Orwell',
+                publishedYear: 1949
+            },
+            {
+                title: 'To Kill a Mockingbird',
+                author: 'Harper Lee',
+                publishedYear: 1960
+            },
+            {
+                title: 'The Great Gatsby',
+                author: 'F. Scott Fitzgerald',
+                publishedYear: 1925
+            }
+        ]);
+        console.log('📚 Seed data inserted');
+    } else {
+        console.log('✅ Seed data already exists. Skipping...');
+    }
+};
+
+module.exports = seedBooks;
