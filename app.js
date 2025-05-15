b9e5d042ceb791c81f5b953e00260d9c35c869cb
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-05-15 07:05:04 +0300

    Updated Error Handling Middleware ✔

--- app.js
+++ app.js
@@ -52,14 +52,16 @@ app.put('/api/books/:id', booksController.updateBook);
 app.delete('/api/books/:id', booksController.deleteBook);
 
 //! Not Found Middleware
-app.use((request, response, next) => {
-    response.status(404).json({ message: "Not Found" });
+app.use((req, res, next) => {
+    res.status(404).json({ message: "Not Found" });
 });
 
 //! Error Middleware
-app.use((error, request, response, next) => {
-    // console.log(error)
-    response.status(500).json({ message: `Exception Occured: ${error}` });
+app.use((error, req, res, next) => {
+    res.status(500).json({
+        message: 'Something went wrong',
+        details: error.message,
+    });
 });
 
 //! Start the server
